0eeb82dc916b20966c71ff9b09fd4f34ff6fb58c
Amin Aouina <amine.aouina.lp.4@gmail.com>
2024-03-21 23:57:29 +0100

    unit tests

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -3,6 +3,7 @@ using HotelBooking.Core;
 using HotelBooking.UnitTests.Fakes;
 using Xunit;
 using System.Linq;
+using System.Collections.Generic;
 
 
 namespace HotelBooking.UnitTests
@@ -66,5 +67,77 @@ namespace HotelBooking.UnitTests
             Assert.Empty(bookingForReturnedRoomId);
         }
 
+
+        [Fact]
+        public void CreateBooking_ValidBooking_ReturnsTrue()
+        {
+            // Arrange
+            var booking = new Booking
+            {
+                StartDate = DateTime.Today.AddDays(12),
+                EndDate = DateTime.Today.AddDays(15)
+            };
+
+            // Act
+            bool result = bookingManager.CreateBooking(booking);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CreateBooking_RoomUnavailable_ReturnsFalse()
+        {
+            // Arrange
+            var booking = new Booking
+            {
+                StartDate = DateTime.Today.AddDays(12),
+                EndDate = DateTime.Today.AddDays(15)
+            };
+
+            // Book all rooms for the test period
+            foreach (var room in bookingRepository.GetAll().Select(b => b.RoomId).Distinct())
+            {
+                bookingRepository.Add(new Booking
+                {
+                    StartDate = booking.StartDate,
+                    EndDate = booking.EndDate,
+                    RoomId = room,
+                    IsActive = true
+                });
+            }
+
+            // Act
+            bool result = bookingManager.CreateBooking(booking);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GetFullyOccupiedDates_ValidDates_ReturnsOccupiedDates()
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(12);
+            DateTime endDate = DateTime.Today.AddDays(15);
+
+            // Book all rooms for the test period
+            foreach (var room in bookingRepository.GetAll().Select(b => b.RoomId).Distinct())
+            {
+                bookingRepository.Add(new Booking
+                {
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    RoomId = room,
+                    IsActive = true
+                });
+            }
+
+            // Act
+            List<DateTime> fullyOccupiedDates = bookingManager.GetFullyOccupiedDates(startDate, endDate);
+
+            // Assert
+            Assert.Equal(4, fullyOccupiedDates.Count); // 4 days fully occupied
+        }
     }
 }
